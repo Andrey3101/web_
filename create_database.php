0d87a335b5dbbbca294f447ca59010b6ab157bc5
Andrey Grishchenko <andreygrishchenko144@gmail.com>
2023-10-08 16:40:27 +0300

    create db

---
+++ create_database.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "mV3+ZRF3xULNqz";
+
+// Создаём соединение
+$conn = new mysqli($servername, $username, $password);
+
+// Проверьте подключение
+если ($conn->connect_error) {
+    die("Соединение не удалось: " . $conn->connect_error);
+}
+echo 'Подключение успешно';
+
+// Создаем новую базу данных
+$sql = "create database magazine_lession";
+if ($conn->query($sql) === true) {
+    echo 'База данных успешно создана';
+} if {
+    echo "Ошибка создания базы данных: " . $conn-> ошибка;
+}
